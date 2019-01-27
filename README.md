a3bfbf0d69946f5d921cb62d17ae23698bd6820b
Rob Brennan <rob@therobbrennan.com>
2019-01-26 22:22:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome
+This project is based off [How to Deploy Express on Now.sh](https://dev.to/warenix/how-to-deploy-express-on-nowsh-414i).
---
+++ index.js
@@ -0,0 +1,24 @@
+const port = process.env.PORT || 3000
+
+var express = require('express')
+var app = express()
+app.use(express.json())
+
+app.listen(port, err => {
+  if (err) throw err
+  console.log(`> Ready on server http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('ExpressJS server is online.')
+})
+
+app.get('/get', (req, res, next) => {
+  res.json({
+    version: process.env.VERSION,
+  })
+})
+
+app.post('/post', function(request, response) {
+  response.send(request.body)
+})
